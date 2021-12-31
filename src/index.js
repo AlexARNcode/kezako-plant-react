8ce5dddad1baf2d5371c34e15fc6a557b6b06f57
AlexARNcode <alexarnout@gmail.com>
2021-12-31 10:50:11 +0100

    fix css link

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap-sketchy.min.css"
 
 ReactDOM.render(
   <React.StrictMode>
