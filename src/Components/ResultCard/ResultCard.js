a3939001531d6dda8b3cc9e15c79ab4698038328
AlexARNcode <alexarnout@gmail.com>
2022-01-07 20:11:26 +0100

    Add keys to list

--- src/Components/ResultCard/ResultCard.js
+++ src/Components/ResultCard/ResultCard.js
@@ -2,23 +2,25 @@ import React from "react";
 
 export default function ResultCard(props) {
   return (
-    <div className="card border-info mb-3">
-      <div className="card-header">
-        <h5 className="card-title">{props.number}</h5>
+    <>
+      <div className="card border-info mb-3">
+        <div className="card-header">
+          <h5 className="card-title">{props.number}</h5>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Score : {props.score} %</p>
+          <p className="card-text">Scientific Name : {props.scientificName}</p>
+          {props.commonNames.length !== 0 ? (
+            <p className="card-text">
+              Common Names : {props.commonNames.join(", ")}
+            </p>
+          ) : (
+            <p className="card-text">
+              Common Names : <span className="text-muted">Not found</span>
+            </p>
+          )}
+        </div>
       </div>
-      <div className="card-body">
-        <p className="card-text">Score : {props.score} %</p>
-        <p className="card-text">Scientific Name : {props.scientificName}</p>
-        {props.commonNames.length !== 0 ? (
-          <p className="card-text">
-            Common Names : {props.commonNames.join(", ")}
-          </p>
-        ) : (
-          <p className="card-text">
-            Common Names : <span className="text-muted">Not found</span>
-          </p>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,23 +1,25 @@
 import React from "react";
 import ResultCard from "../ResultCard/ResultCard";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Results(props) {
   return (
-    <div>
-      {props.results[1] && <h2 className="text-center mt-4 mb-3">Results</h2>}
+    <>
+      <ul>
       {props.results[1] &&
         props.results[1].map((result, i) => {
           return (
-            <>
+            <li key={uuidv4()} style={{listStyleType: "none"}}>
               <ResultCard
                 score={Math.round(result.score * 100)}
                 scientificName={result.species.scientificName}
                 commonNames={result.species.commonNames}
                 number={i + 1}
               />
-            </>
+            </li>
           );
         })}
-    </div>
+        </ul>
+    </>
   );
 }
