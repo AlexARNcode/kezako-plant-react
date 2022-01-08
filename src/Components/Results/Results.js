1cdb3a49ea6ca3c5567444a4c7c8e78c18df79b7
AlexARNcode <alexarnout@gmail.com>
2022-01-08 10:27:53 +0100

    Remove left margin from list

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,25 +1,25 @@
 import React from "react";
 import ResultCard from "../ResultCard/ResultCard";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 export default function Results(props) {
   return (
     <>
-      <ul>
-      {props.results[1] &&
-        props.results[1].map((result, i) => {
-          return (
-            <li key={uuidv4()} style={{listStyleType: "none"}}>
-              <ResultCard
-                score={Math.round(result.score * 100)}
-                scientificName={result.species.scientificName}
-                commonNames={result.species.commonNames}
-                number={i + 1}
-              />
-            </li>
-          );
-        })}
-        </ul>
+      <ul id="results-list">
+        {props.results[1] &&
+          props.results[1].map((result, i) => {
+            return (
+              <li key={uuidv4()} id="results-item">
+                <ResultCard
+                  score={Math.round(result.score * 100)}
+                  scientificName={result.species.scientificName}
+                  commonNames={result.species.commonNames}
+                  number={i + 1}
+                />
+              </li>
+            );
+          })}
+      </ul>
     </>
   );
 }
